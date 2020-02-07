58c4ce3ecf3ea1dedccff4256d843348241c7564
Sam Deane <sam@elegantchaos.com>
2020-02-07 16:19:07 +0000

    Fixed visibility.

--- Sources/Performance/Profiler.swift
+++ Sources/Performance/Profiler.swift
@@ -3,15 +3,20 @@
 //  All code (c) 2020 - present day, Elegant Chaos Limited.
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
+import Darwin
+
 public struct Profiler {
     let start = clock_gettime_nsec_np(CLOCK_UPTIME_RAW)
     
-    var elapsedNanoseconds: UInt {
+    public init() {
+    }
+    
+    public var elapsedNanoseconds: UInt {
         let finish = clock_gettime_nsec_np(CLOCK_UPTIME_RAW)
         return UInt(finish - start)
     }
     
-    var elapsed: Double {
+    public var elapsed: Double {
         let finish = clock_gettime_nsec_np(CLOCK_UPTIME_RAW)
         return Double(finish - start) / 1000000000.0
     }
